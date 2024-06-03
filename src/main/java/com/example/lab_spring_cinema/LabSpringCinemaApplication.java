336b8c8b408510b1d8b093cceb4317af322c9622
Rabin Kandel <kandelrabin@live.com>
2024-06-03 13:45:04 +0100

    Create lab_spring_cinema repo

---
+++ src/main/java/com/example/lab_spring_cinema/LabSpringCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_spring_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab_spring_cinema/LabSpringCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_spring_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
